5ca3dcd2d2c4e158dada6cce6e9ecbd8da60ccfb
Kot19 <kolya.kovalec@ukr.net>
2016-08-31 21:34:47 +0300

    Bank2Commit

---
+++ src/Found.java
@@ -0,0 +1,12 @@
+
+public class Found {
+    public static void main(String[] args) {
+        int withdrow = 100;
+        int[] balance = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+        int[] balance1 = new int[3];
+
+        System.out.println("Oww " + (balance[3] +  withdrow) +"$" );
+        }
+    }
+
